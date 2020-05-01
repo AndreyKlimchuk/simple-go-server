12f28cac5554e2fd5528de78a7513d125cdbb009
andrew <ua.challenger@gmail.com>
2020-05-01 15:17:06 +0300

    Improvement of logging and tests

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
     "net/http"
     "html/template"
     "sync/atomic"
+    "flag"
+    "strconv"
+    "net"
 )
 
 var indexTemplate = template.Must(template.ParseFiles("index.html"))
@@ -15,14 +18,28 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		atomic.AddUint64(&visitorsCount, 1)
 		err := indexTemplate.Execute(w, visitorsCount)
 		if err != nil {
-	        http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Println(err)
 	    }
 	} else {
 		http.NotFound(w, r)
 	}
 }
 
+func start_http_server(listener net.Listener) {
+	http.HandleFunc("/", handler)
+    log.Fatal(http.Serve(listener, nil))
+}
+
+func start_listener(port int) (net.Listener) {
+	listener, err := net.Listen("tcp", ":" + strconv.Itoa(port))
+	if err != nil {
+	    log.Fatal(err)
+	}
+	return listener;
+}
+
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	port := flag.Int("port", 8080, "port number")
+	flag.Parse()
+	start_http_server(start_listener(*port))
 }
--- main_test.go
+++ main_test.go
@@ -3,15 +3,78 @@ package main
 import (
 	"testing"
 	"net/http"
+	"strconv"
+	"sync"
+	"os"
+	"time"
+	"net"
 )
 
-func TestMain(t *testing.T) {
-	go main()
-	resp, err := http.Get("http://localhost:8080/") 
+import "github.com/PuerkitoBio/goquery"
+
+var port string;
+
+func TestMain(m *testing.M) {
+	listener := start_listener(0) // start listener on available port
+	port = strconv.Itoa(listener.Addr().(*net.TCPAddr).Port)
+	go start_http_server(listener)
+	time.Sleep(1 * time.Second) // waiting server initialization
+	os.Exit(m.Run())
+}
+
+func TestSequential(t *testing.T) {
+	countOld := get_visitors_count(t)
+	for i := 0; i < 10; i++ {
+		countNew := get_visitors_count(t)
+		if countOld + 1 != countNew {
+			t.Fatalf("Invalid visitors count increment: %d -> %d", countOld, countNew)
+		}
+		countOld = countNew
+	}
+}
+
+func TestParallel(t *testing.T) {
+	var wg sync.WaitGroup
+
+	// Trying to achive race condition when counter is not atomic
+	countInit := get_visitors_count(t)
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go func() {
+			for c := 0; c < 100; c++ {
+				request_page(t)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	countFinal := get_visitors_count(t)
+	if countInit + 401 != countFinal {
+		t.Fatalf("Invalid visitors count increment: %d -> %d", countInit, countFinal)
+	}
+}
+
+func request_page(t *testing.T) (*http.Response) {
+	resp, err := http.Get("http://localhost:" + port +"/")
 	if err != nil { 
-        t.Fatalf("Request failed") 
-    }
-    if resp.StatusCode != 200 {
-    	t.Fatalf("Bad response code: %d", resp.StatusCode) 
-    } 
+		t.Fatal(err) 
+	}
+	if resp.StatusCode != 200 {
+		t.Fatalf("Bad response code: %d", resp.StatusCode) 
+	}
+	return resp  
+}
+
+func get_visitors_count(t *testing.T) (int) {
+	resp := request_page(t) 
+	defer resp.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	visitorsCount, err := strconv.Atoi(doc.Find("#visitors-count").Text())
+	if err != nil {
+		t.Fatal(err)
+	}
+	return visitorsCount
 }
